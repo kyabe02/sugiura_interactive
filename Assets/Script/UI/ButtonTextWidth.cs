76a3af5fabf5ea8389c490837b8cb8855bb524dd
kyabe02 <48001967+kyabe02@users.noreply.github.com>
2020-12-15 12:44:49 +0900

    コンフリクト防止まーじ

--- Assets/Script/UI/ButtonTextWidth.cs
+++ Assets/Script/UI/ButtonTextWidth.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonTextWidth : MonoBehaviour
 {
+    Text text;
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = GetComponentInChildren<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void TextWidthChange()
+    {
+        //text.w
     }
 }
